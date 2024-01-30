f1ba67771469a1d4e1bb143302f838443a353a45
Marisahdz <143449206+Marisahdz@users.noreply.github.com>
2024-01-30 11:34:33 -0800

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,15 @@
+
+document.getElementById('Biotechnology').addEventListener('click', function() {
+    
+    window.location.href = 'Biotech.html';
+});
+
+document.getElementById('PublicSafety').addEventListener('click', function() {
+    
+    window.location.href = 'PublicSafety.html';
+});
+
+document.getElementById('Software').addEventListener('click', function() {
+    
+    window.location.href = 'Software.html';
+});
